2fcf7ed5bb7b321e59f53b590390c8a099a94fe2
Nima <128128839+nimahadi1388@users.noreply.github.com>
2026-07-08 22:29:57 +0330

    4thClass&1stExercise

--- src/assets/components/Timer.jsx
+++ src/assets/components/Timer.jsx
@@ -1,22 +1,33 @@
 import React from "react";
-
+let timeOut;
 class Timer extends React.Component {
   constructor() {
     super();
     this.state = {
-      timer: new Date().toLocaleTimeString(),
+      count: 10,
     };
   }
-  render() {
-    setInterval(() => {
+  componentDidMount() {
+    console.log('mamad');
+    
+    timeOut = setInterval(() => {
       this.setState({
-        timer: new Date().toLocaleTimeString(),
+        count: this.state.count - 1,
       });
     }, 1000);
+  }
+  componentDidUpdate(){
+    if(this.state.count == 0){
+      clearInterval(timeOut)
+    }
+  }
+  render() {
+    console.log('amat');
+    
     return (
       <div>
-        <h1>ساعت لحظه ای</h1>
-        <p>{this.state.timer}</p>
+        <h1>شمارش معکوس</h1>
+        <p>{this.state.count}</p>
       </div>
     );
   }
