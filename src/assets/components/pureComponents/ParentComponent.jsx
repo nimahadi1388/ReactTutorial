979fff30c2760572aa2bae6954b024f0058a05df
Nima <128128839+nimahadi1388@users.noreply.github.com>
2026-07-18 20:20:15 +0330

    PureComponents-1

--- src/assets/components/pureComponents/ParentComponent.jsx
+++ src/assets/components/pureComponents/ParentComponent.jsx
@@ -1,12 +1,27 @@
+// -------------- Class Type -----------------
 import React from "react";
 import PureComponents from "./PureComponent";
 class ParentComponent extends React.Component {
-  render() {
-    return (
-      <>
-        <PureComponents />
+    constructor() {
+        super();
+        this.state = {
+            name: "Nima",
+        };
+    }
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                name: `Nima ${new Date().toLocaleTimeString()}`,
+            });
+        }, 1000);
+    }
+    render() {
+        console.log("--------------------Parent components--------------------");
+        return (
+            <>
+        <PureComponents name={this.state.name}/>
       </>
     );
-  }
+}
 }
 export default ParentComponent;
--- src/assets/components/pureComponents/PureComponent.jsx
+++ src/assets/components/pureComponents/PureComponent.jsx
@@ -1,11 +1,27 @@
-import React from "react";
-class PureComponents extends React.Component{
-    render(){
-        return(
-            <>
-                <h1>Mamad</h1>
-            </>
-        )
-    }
-}
-export default PureComponents
+// -------------- Class Type -----------------
+
+import { memo } from "react";
+
+// import React from "react";
+// class PureComponents extends React.PureComponent{
+//     render(){
+//         console.log('Pure components');
+
+//         return(
+//             <>
+//                 <h1>Pure Components: {this.props.name}</h1>
+//             </>
+//         )
+//     }
+// }
+// export default PureComponents
+
+// -------------- Function Type -----------------
+const PureComponents = (props) => {
+  return (
+    <>
+      <h1>Pure Components: {props.name}</h1>
+    </>
+  );
+};
+export default memo(PureComponents);
